8ac11e35decdf2a0a39e429a75441e41d25c7e61
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-09-18 14:22:39 +0300

    tags; wrapping

--- src/Graph.cr
+++ src/Graph.cr
@@ -7,24 +7,50 @@ module Wool
     def initialize(@sweater)
     end
 
+    protected def wrap(text : String, width : UInt16)
+      words = text.split
+      lines = [] of String
+      current_line = ""
+
+      words.each do |word|
+        if current_line.empty?
+          current_line = word
+        elsif current_line.size + word.size + 1 <= width
+          current_line += " " + word
+        else
+          lines << current_line
+          current_line = word
+        end
+      end
+
+      lines << current_line unless current_line.empty?
+      lines.join "\\n"
+    end
+
     def write(io : IO)
       io << "digraph sweater {"
       @sweater.chest.oids do |oid|
+        id = Id.from_oid oid
+        ids = id.to_string
         c = Content.from_json (@sweater.chest.get oid).not_nil!["content"].to_json
-        id = (Id.from_oid oid).to_string
+        t = @sweater.index.get id.to_bytes
+        tags = (t.size > 0) ? wrap (t.join ' '), 20 : nil
         case c
         when String
-          io << "\n\t\"#{id}\" [label=\"#{c}\", shape=box];"
+          text = wrap c, 20
+          label = tags ? "{#{text}|#{tags}}" : text
+          io << "\n\t\"#{id.to_string}\" [label=\"#{label}\", shape=record];"
         when Relation
-          label = c[:type].to_s.underscore.gsub '_', ' '
-          fid = c[:from].to_string
-          tid = c[:to].to_string
-          iid = "#{fid} -> #{tid}"
-          io << "\n\t\"#{iid}\" [label=\"\", style=invis, fixedsize=\"false\", width=0, height=0, shape=none]"
-          io << "\n\t\"#{fid}\" -> \"#{iid}\" [arrowhead=none];"
-          io << "\n\t\"#{iid}\" -> \"#{tid}\";"
-          io << "\n\t\"#{id}\" [label=\"#{label}\", shape=ellipse];"
-          io << "\n\t\"#{iid}\" -> \"#{id}\" [dir=none, style=dotted];"
+          text = c[:type].to_s.underscore.gsub '_', ' '
+          label = tags ? "{#{text}|#{tags}}" : text
+          fids = c[:from].to_string
+          tids = c[:to].to_string
+          iids = "#{fids} -> #{tids}"
+          io << "\n\t\"#{iids}\" [label=\"\", style=invis, fixedsize=\"false\", width=0, height=0, shape=none]"
+          io << "\n\t\"#{fids}\" -> \"#{iids}\" [arrowhead=none];"
+          io << "\n\t\"#{iids}\" -> \"#{tids}\";"
+          io << "\n\t\"#{ids}\" [label=\"#{label}\", shape=record, style=rounded];"
+          io << "\n\t\"#{iids}\" -> \"#{ids}\" [dir=none, style=dotted];"
         end
       end
       io << "\n}"
